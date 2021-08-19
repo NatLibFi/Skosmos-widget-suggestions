6ae12771894950f4af88cf956d89649e3609becc
Ahonen Mika J <mika.ahonen@helsinki.fi>
2021-08-19 15:03:37 +0300

    added node_modules

--- component-bundler/temporaryUpdaterToPatchWebPackIssue.sh
+++ component-bundler/temporaryUpdaterToPatchWebPackIssue.sh
@@ -1,8 +1,8 @@
 #!/bin/bash 
 sudo chown ${USER}:[YOUR_GROUP] dist/components.js
-sudo chmod 777 ../components.js dist/components.js
+sudo chmod 744 ../components.js dist/components.js
 npm run build
-sudo chmod 777 -R dist
+sudo chmod 744 -R dist
 cp -p dist/components.js ../
 ls -la ../components.js 
 
