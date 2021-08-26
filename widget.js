a3e3346c1d573ab62f6e9369508b0f7a721d067f
Ahonen Mika J <mika.ahonen@helsinki.fi>
2021-08-26 22:44:16 +0300

    Improved direct link options

--- widget.js
+++ widget.js
@@ -4,40 +4,38 @@ var SUGGESTIONS = SUGGESTIONS || {};
 const delay = ms => new Promise(resolved => setTimeout(resolved, ms));
 
 SUGGESTIONS = {
-    renderNew: async function() {
-        $("#vocab-info").append(Handlebars.compile( $("#suggestions-new").html())(
-            {
-                "url": this.fetchUrl() ? this.fetchUrl() : '',
-                "lang": lang ? lang : '',
-                "vocab": vocab ? vocab : ''
-            }
-            ));
-        await delay(2000);
-        window.location.href.includes("#directnew") ? window.document.getElementById("fordirectnew").click() : '';
-        },
+    renderNew: function() {
+        $("#vocab-info").append(Handlebars.compile( $("#suggestions-new").html())({
+            "url": this.fetchUrl() ? this.fetchUrl() : '',
+            "lang": lang ? lang : '',
+            "vocab": vocab ? vocab : ''
+            }));
+        window.onload = async function exampleFunction() {
+            await delay(2000);
+            window.location.href.includes("#directnew") ? window.document.getElementById("fordirectnew").click() : '';
+        }
+    },
+
     renderChange: async function(data) {
-        $(".concept-main").append(Handlebars.compile( $("#suggestions-change").html())(
-            {
-                "url": this.fetchUrl() ? this.fetchUrl() : '',
-                "lang": lang ? lang : '',
-                "vocab": vocab ? vocab : '',
-                "label": data.prefLabels[0].label ? data.prefLabels[0].label : '',
-                "uri": this.fetchUri(data) ? this.fetchUri(data) : ''
-            }
-            ));
+        $(".concept-main").append(Handlebars.compile( $("#suggestions-change").html())({
+            "url": this.fetchUrl() ? this.fetchUrl() : '',
+            "lang": lang ? lang : '',
+            "vocab": vocab ? vocab : '',
+            "label": data.prefLabels[0].label ? data.prefLabels[0].label : '',
+            "uri": this.fetchUri(data) ? this.fetchUri(data) : ''
+            }));
         await delay(2000);
         window.location.href.includes("#directmodify") ? window.document.getElementById("fordirectmodify").click() : '';
-        },
+    },
             
-            fetchUri: data => data.uri ? data.uri : '',
-            fetchUrl: () => window.location.href
-            
-        };
+    fetchUri: data => data.uri ? data.uri : '',
+    fetchUrl: () => window.location.href
+};
         
-        $(function() {
-            window.suggestionsWidget = function(data) {
-                $('#vocab-info').length > 0 ? SUGGESTIONS.renderNew() : '';
-                $('.concept-main').length > 0 ? SUGGESTIONS.renderChange(data) : '';
-            };
-        });
+$(function() {
+    window.suggestionsWidget = function(data) {
+        $('#vocab-info').length > 0 ? SUGGESTIONS.renderNew() : '';
+        $('.concept-main').length > 0 ? SUGGESTIONS.renderChange(data) : '';
+    };
+});
         
