e8d2e98feee4517dd4c0d2bd93959d10bb108daa
UnniKohonen <45235116+UnniKohonen@users.noreply.github.com>
2025-08-06 14:57:41 +0300

    Add fields to change form

--- plugin/components/suggestion-change-form.js
+++ plugin/components/suggestion-change-form.js
@@ -1,10 +1,26 @@
 SUGGESTION_PLUGIN.suggestionChangeFormComponent = {
   data() {
-    return {}
+    return {
+      change: '',
+      explanation: '',
+      organization: ''
+    }
   },
   template: `
     <div id="suggestion-form" role="form">
       <div id="suggestion-form-inputs">
+        <basic-textarea
+          :label="{text: 'Ehdotettu muutos:', id: 'suggestion-change'}"
+          v-model:text="change"
+        ></basic-textarea>
+        <basic-textarea
+          :label="{text: 'Perustelut ehdotukselle:', id: 'suggestion-explanation'}"
+          v-model:text="explanation"
+        ></basic-textarea>
+        <basic-input
+          :label="{text: 'Ehdottajan organisaatio:', id: 'suggestion-organization'}"
+          v-model:text="organization"
+        ></basic-input>
       </div>
     </div>
   `
