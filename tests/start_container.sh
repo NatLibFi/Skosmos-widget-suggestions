ebab9687d310970bc7f453069ad4ccbdf985a8fe
UnniKohonen <45235116+UnniKohonen@users.noreply.github.com>
2025-09-11 12:28:21 +0300

    Use github workspace path in script

--- tests/start_container.sh
+++ tests/start_container.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+if [[ -n "${GITHUB_WORKSPACE}" ]]; then
+    BASE_PATH="${GITHUB_WORKSPACE}"
+else
+    BASE_PATH="."
+fi
+
+echo "$BASE_PATH"
+
 docker run -d -p 9090:80 \
-  -v ./testconfig.ttl:/var/www/html/config.ttl \
-  -v ../plugin/:/var/www/html/plugins/suggestion-plugin/ \
+  -v "$BASE_PATH/tests/testconfig.ttl:/var/www/html/config.ttl" \
+  -v "$BASE_PATH/plugin/:/var/www/html/plugins/suggestion-plugin/" \
   quay.io/natlibfi/skosmos
