ff5482cdd4caa31c14bb7dfe4df1d769d26b17e0
UnniKohonen <45235116+UnniKohonen@users.noreply.github.com>
2025-09-11 14:46:46 +0300

    Remove echo from container script

--- tests/start_container.sh
+++ tests/start_container.sh
@@ -6,8 +6,6 @@ else
     BASE_PATH="."
 fi
 
-echo "$BASE_PATH"
-
 docker run -d -p 9090:80 \
   -v "$BASE_PATH/tests/testconfig.ttl:/var/www/html/config.ttl" \
   -v "$BASE_PATH/plugin/:/var/www/html/plugins/suggestion-plugin/" \
