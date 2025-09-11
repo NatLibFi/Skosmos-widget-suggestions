99940da0b8e238c2c946355e2f6a948198af4574
UnniKohonen <45235116+UnniKohonen@users.noreply.github.com>
2025-09-11 12:10:14 +0300

    Fix file paths

--- tests/start_container.sh
+++ tests/start_container.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 docker run -d -p 9090:80 \
-  -v ./tests/testconfig.ttl:/var/www/html/config.ttl \
-  -v ./plugin/:/var/www/html/plugins/suggestion-plugin/ \
+  -v ./testconfig.ttl:/var/www/html/config.ttl \
+  -v ../plugin/:/var/www/html/plugins/suggestion-plugin/ \
   quay.io/natlibfi/skosmos
